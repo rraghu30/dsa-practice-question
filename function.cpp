968296b10abf47671ce70da98551ac038590207e
rraghu30 <rohitraghav4544@gmail.com>
2024-11-19 10:57:33 +0530

    committed

--- function.cpp
+++ function.cpp
@@ -68,6 +68,8 @@ int main() {
 
 */
 
+
+/*
 int factorialN(int n){
     int fact = 1;
 
@@ -84,3 +86,74 @@ int main(){
 
     return 0;
 }
+
+*/
+
+
+/*
+int sum(int a, int b){
+    a = a + 10;
+    b = b + 10;
+    return a + b;
+}
+
+int main(){
+    int a = 5, b = 4;
+    cout << sum(a,b) <<endl;
+
+    cout << a <<endl;
+    cout << b <<endl;
+    return 0;
+}
+
+*/
+
+// sum of digit of a number
+
+/*
+int sumOfDigit(int num){
+    int digSum = 0;
+
+    while(num > 0){
+        int lastDig = num % 10;
+        num /= 10;
+
+        digSum += lastDig; 
+    }
+
+    return digSum;
+}
+
+int main(){
+    cout<<"sum = " <<sumOfDigit(2345) <<endl;
+
+    return 0;
+}
+
+*/
+
+ int factorial(int n){
+    int fact = 1;
+
+    for(int i=1; i<=n; i++){
+        fact *=i;
+    }
+    return fact;
+ }
+
+ int nCr(int n, int r){
+    int fact_n = factorial(n);
+    int fact_r = factorial(r);
+    int fact_nmr = factorial(n-r);
+
+    return fact_n / (fact_r * fact_nmr);
+
+ }
+
+ int main(){
+    int n = 6, r =3;
+    cout<< nCr(n, r) << endl;
+
+    return 0;
+ }
+
