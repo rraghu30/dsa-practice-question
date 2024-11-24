98d8502151e5971a106649e56c0bb70662f40733
rraghu30 <rohitraghav4544@gmail.com>
2024-11-24 22:50:31 +0530

    committed

--- function.cpp
+++ function.cpp
@@ -132,6 +132,8 @@ int main(){
 
 */
 
+/*
+
  int factorial(int n){
     int fact = 1;
 
@@ -157,4 +159,30 @@ int main(){
     return 0;
  }
 
+ */
+
 // hy , I am Raghu.
+
+
+int decToBinary(int decNum) {
+    int ans =0, pow = 1;
+
+    while(decNum > 0) {
+        int rem = decNum % 2;
+        decNum /= 2;
+
+         ans +=(rem * pow);
+         pow *= 10;
+    }
+
+    return ans;  // Binay number
+}
+
+int main(){
+    int decNum = 50;
+
+    for(int i=1; i<=10; i++){
+        cout << decToBinary(i) << endl;
+    }
+    return 0;
+}
